817a85bd6f3d57d23d3ec86aba5d65c23a43402d
Ira Mishra <Ira@local.dev>
2026-09-23 23:57:05 +0530

    Added a custom tip-calculator logic

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,5 @@
-bmi = 84 / 1.65 ** 2
+# f-strings
+name = input('What is your name?\n')
+userYear = input("Which year were you born in?\n")
+age = 2026 - int(userYear)
+print(f"Hi {name}! You're probably {age} years old.")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -1,6 +1,14 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
-people = int(input("How many people to split the bill? "))
-
-
+print("Welcome to the Tip Calculator!")
+bill = input("What was the total bill?\n $")
+percentage = input("How much tip would you like to give? 10, 12 or 15 percent\n ")
+bill = float(bill)
+if (percentage == "10"):
+    tip = bill * (10/100)
+elif (percentage == "12"):
+    tip = bill * (12/100)
+elif (percentage == "15"):
+    tip = bill * (15/100)
+Num_of_people = input("How many people do you have?\n")
+bill_per_person = tip/int(Num_of_people)
+bill_per_person = round(bill_per_person,2)
+print(f"Each person should pay: ${bill_per_person}")
